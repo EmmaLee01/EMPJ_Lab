c8c36404d5c2aa1dee6ccf05bdff68196ae0c37b
paulzhao11 <bcc3yx@virginia.edu>
2025-01-30 15:58:16 -0500

    Update run_drive.R

--- run_drive.R
+++ run_drive.R
@@ -1,11 +1,17 @@
 # Drive function
 
-run_drive <- function(down, ytg, fp) {
+run_drive <- function(state) {
   max <- 100 - state$fp
   min <- -state$fp
   yard_change <- sample(min:max, 1)
   state$fp <- state$fp + yard_change
   state$fp <- max(0, min(100, state$fp)) #in bound
   return(state)
+
+  if (state$fp >= 100) {
+    state$score <- ifelse(state$fp >= 110, 3, 7)
+    } else {
+    state$score <- 0
+  }
 }
   
