ea743abc660b9f567a75e1d75e3b6f13394dae15
Emily (Chuyun) Arbieto <55031383+EmmaLee01@users.noreply.github.com>
2025-01-30 16:28:10 -0500

    Update topfunc.R

--- topfunc.R
+++ topfunc.R
@@ -2,7 +2,7 @@ top_func <- function(downs, ytg, fp, epochs=1000) { ##top function is basically 
   scores <- epochs ##placeholder for counting scores with the 
   for(i in 1:epochs) {
     state <- list(down = 1, ytg = 10, fp = sample(1:100, 1))
-    scores[i] <- run_epoch(downs, ytg, fp)
+    scores[i] <- run_epoch(state)
     
   }
   mean(scores) ##calculate score, expected points
