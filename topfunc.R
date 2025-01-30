2ac55ea17317afed30c7e5014339d9ed62ab8169
Emily (Chuyun) Arbieto <55031383+EmmaLee01@users.noreply.github.com>
2025-01-30 16:21:22 -0500

    Update topfunc.R

--- topfunc.R
+++ topfunc.R
@@ -1,6 +1,7 @@
 top_func <- function(downs, ytg, fp, epochs=1000) { ##top function is basically supposed to run both epoch function and the drive, prof. rodu asked to make this a long number
   scores <- epochs ##placeholder for counting scores with the 
   for(i in 1:epochs) {
+    state <- list(down = 1, ytg = 10, fp = sample(1:100, 1))
     scores[i] <- run_epoch(downs, ytg, fp)
     
   }
