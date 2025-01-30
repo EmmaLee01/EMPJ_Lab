539e32ed7b04b7766ccd0222c1681cc798edbe97
jchen925 <148140603+jchen925@users.noreply.github.com>
2025-01-30 15:03:21 -0500

    Update topfunc.R

--- topfunc.R
+++ topfunc.R
@@ -1,7 +1,7 @@
 top_func <- function(downs, ytg, fp, epochs=1000) {
   scores <- epochs
   for(i in 1:epochs) {
-    scores[i] <- simulate_epoch(downs, ytg, fp)
+    scores[i] <- run_epoch(downs, ytg, fp)
     
   }
   mean(scores)
