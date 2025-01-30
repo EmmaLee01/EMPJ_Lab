4f8d240d228ed010da1471159583d1c46fd7ba42
Emily (Chuyun) Arbieto <55031383+EmmaLee01@users.noreply.github.com>
2025-01-30 15:08:00 -0500

    Update topfunc.R

--- topfunc.R
+++ topfunc.R
@@ -1,8 +1,8 @@
-top_func <- function(downs, ytg, fp, epochs=1000) {
-  scores <- epochs
+top_func <- function(downs, ytg, fp, epochs=1000) { ##top function is basically supposed to run both epoch function and the drive
+  scores <- epochs ##placeholder for counting scores with the 
   for(i in 1:epochs) {
     scores[i] <- run_epoch(downs, ytg, fp)
     
   }
-  mean(scores)
+  mean(scores) ##calculate score
 }
