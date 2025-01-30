c25488fd5da295a27b39aec8dd780988ceb41db4
paulzhao11 <bcc3yx@virginia.edu>
2025-01-30 16:06:40 -0500

    Update run_drive.R

--- run_drive.R
+++ run_drive.R
@@ -7,5 +7,19 @@ run_drive <- function(state) {
   state$fp <- state$fp + yard_change
   state$fp <- max(0, min(100, state$fp)) #in bound
   return(state)
+  
+  if (state$ytg <= 0) {
+    state$down <- 1   # back to first down
+    state$ytg <- 10
+  } else {
+    state$down <- state$down + 1  # Increment down
+  }
+  
+  if (state$down > 4) {
+    state$down <- 1  # first down opponent
+    state$ytg <- 10
+    state$fp <- 100 - state$fp  # Flip field position
+  }
+  return(state)
 }
   
