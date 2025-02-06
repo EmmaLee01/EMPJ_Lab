3c6252bed16ecec0bac7f713d7c5eadaca793fb7
Maxwell Elim <nam9af@virginia.edu>
2025-02-06 15:50:29 -0500

    Added comments Lab3CoinSim.R

--- Lab3CoinSim.R
+++ Lab3CoinSim.R
@@ -16,7 +16,7 @@ current_team <- sample(teams, size = 1)  # Initialize with a random team
 
 
 for(i in 1:coinpass){ ##For loop
-  prob_dist<-transition_matrix[current_team,] ##assigning probability distribution for new coin holder
+  prob_dist<-transition_matrix[current_team,] ##assigning probability distribution for new coin holder using transition matrix from other file
   current_team <- sample(1:teamlen, size=1, prob=prob_dist) ##sampling new team from specific team probability distribution
   if (i > ignore) { ##loop for when to start adding possessions
     visits[current_team] <- visits[current_team] + 1
