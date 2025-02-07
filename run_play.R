a8f00182f65f65be9a4907a4e51736c942cf9ff7
Maxwell Elim <nam9af@virginia.edu>
2025-02-07 10:19:07 -0500

    Update run_play.R
    Added a possible down_one helper function

--- run_play.R
+++ run_play.R
@@ -1,3 +1,14 @@
 ## Lab 4
 
 # Run play file 
+
+# Different down functions
+
+down_one <- function(fp, ytg) {
+  # Simulate play outcome (e.g., gain yards, turnover, etc.)
+  gain <- sample(0:10, 1)  #  Random gain between 0 and 10 yards
+  new_position <- fp + gain
+  new_distance <- max(10 - gain, 0)  # Adjust remaining yards for first down
+  list(fp = new_position, ytg = new_distance, down = 2)
+}
+## Possible down function ^
