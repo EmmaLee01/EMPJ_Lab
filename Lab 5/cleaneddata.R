216809b00e3a3d482cb5ec8208c16ccb90a952f1
jchen925 <148140603+jchen925@users.noreply.github.com>
2025-02-27 16:28:54 -0500

    Update cleaneddata.R

--- Lab 5/cleaneddata.R
+++ Lab 5/cleaneddata.R
@@ -1,13 +1,29 @@
-
 footballsub<- footballdata[c(12,19,22,26,29,30)]
-footballsub2<-footballsub[footballsub$down==4,]
+footballsub2<-footballsub[footballsub$drive==4,]
 
 
-                           
 footballsub3<-footballsub2[footballsub2$play_type=="pass"|
-                              footballsub2$play_type=="run"|
-                              footballsub2$play_type=="field_goal"|
-                              footballsub2$play_type=="punt",]
+                             footballsub2$play_type=="run"|
+                             footballsub2$play_type=="field_goal"|
+                             footballsub2$play_type=="punt",]
 footballclean<-na.omit(footballsub3)
-
 footballclean
+
+library(dplyr)
+
+footballclean <- footballclean %>%
+  mutate(play_type = case_when(
+    play_type %in% c("run", "pass") ~ "go_for_it",
+    TRUE ~ play_type  # Keep "punt" and "field_goal" the same
+  ))
+
+# Convert play_type to a factor
+footballclean$play_type <- factor(footballclean$play_type, 
+                                  levels = c("go_for_it", "punt", "field_goal"))
+
+library(nnet)
+
+# Fit multinomial regression model
+multi_model <- multinom(play_type ~ yardline_100 + ydstogo, data = footballclean)
+
+summary(multi_model)
