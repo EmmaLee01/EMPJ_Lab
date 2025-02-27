18b4988b9f37f2da359b5e06ce08c3697f9dedf4
Emily (Chuyun) Arbieto <55031383+EmmaLee01@users.noreply.github.com>
2025-02-27 16:19:19 -0500

    Added Clean Data
    For MLR

---
+++ Lab 5/cleaneddata.R
@@ -0,0 +1,12 @@
+
+footballsub<- footballdata[c(12,19,22,26,29,30)]
+footballsub2<-footballsub[footballsub$drive==4,]
+
+
+                           
+footballsub3<-footballsub2[footballsub2$play_type=="pass"|
+                              footballsub2$play_type=="run"|
+                              footballsub2$play_type=="field_goal"|
+                              footballsub2$play_type=="punt",]
+footballclean<-na.omit(footballsub3)
+footballclean
