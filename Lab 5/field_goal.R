3e9011230258faa6fa85df49ec4b6918ce75d510
Maxwell Elim <nam9af@virginia.edu>
2025-02-27 17:07:43 -0500

    Update field_goal.R

--- Lab 5/field_goal.R
+++ Lab 5/field_goal.R
@@ -13,9 +13,11 @@ fg_data <- football_data2014 %>%
 football_data2014$FG_made <- ifelse(football_data2014$field_goal_result == "made", 1, 0)
 football_data2014$FP <- football_data2014$kick_distance
 fg_model <- glm(FG_made ~ FP, family = binomial, data = football_data2014)
+
 field_goal_probability <- function(FP) {
   log_odds <- predict(fg_model, newdata = data.frame(FP = FP))  
   prob <- exp(log_odds) / (1 + exp(log_odds))  #get probability
   return(unname(prob))
 }
 
+## function from logistic regression file
