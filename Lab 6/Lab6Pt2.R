b3767818aaa1da0943c1d665af1d219187c4eabc
Emily (Chuyun) Arbieto <55031383+EmmaLee01@users.noreply.github.com>
2025-03-06 15:36:26 -0500

    Add files via upload

---
+++ Lab 6/Lab6Pt2.R
@@ -0,0 +1,43 @@
+nhldata<-read.csv("nhl_pbp20162017.csv")
+
+nhlsub<-nhldata[c(1, 6, 7, 8, 11, 12, 54, 55)]
+
+
+
+nhlsub<-nhlsub[!is.na(nhlsub$xC),]
+nhlsub$xC<-as.numeric(nhlsub$xC)
+nhlsub$yC<-as.numeric(nhlsub$yC)
+
+define_region <- function(x, y) {
+  if (x >= 0 & y >= 0) {
+    return("Quad 2")
+  } else if (x >= 0 & y < 0) {
+    return("Quad 4")
+  } else if (x < 0 & y >= 0) {
+    return("Quad 1")
+  } else {
+    return("Quad 3")
+  }
+}
+
+
+
+
+shot_types <- c("SLAP SHOT", "WRIST SHOT")
+nhlsub$Shots <- ifelse(nhlsub$Type %in% shot_types, 1, 0)
+
+nhlsub$region <- mapply(define_region, nhlsub$xC, nhlsub$yC)
+shot_model <- glm(Shots ~ region, data = nhlsub, family = poisson())
+
+
+
+
+
+
+
+
+
+
+
+
+
