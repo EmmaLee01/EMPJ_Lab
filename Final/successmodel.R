997a5d53bb5392cde09de60384a7baa97b43fb96
paulzhao11 <bcc3yx@virginia.edu>
2025-05-01 17:24:08 -0400

    Update successmodel.R

--- Final/successmodel.R
+++ Final/successmodel.R
@@ -25,3 +25,26 @@ ev_actual <- p_success * ep_go + (1 - p_success) * ep_fail
 #Show plots of EP vs FP & YTG
 #Show where risk-adjusted deviations make sense
 #
+
+
+
+
+
+p_success <- predict(success_model, newdata = data.frame(ydstogo = YTG, yardline_100 = FP), type = "response")
+ep_go <- simulate_ep("go_for_it", FP, YTG)
+ep_fg <- replicate(100, attempt_field_goal(FP)) %>% mean()
+ep_punt <- simulate_ep("punt", FP, YTG)
+ep_fail <- simulate_ep("opponent_drive", FP = 100 - FP, YTG = 10)
+
+# Expected value if going for it
+ep_go_expected <- p_success * ep_go + (1 - p_success) * ep_fail
+
+# Break-even success probability
+p_star <- (ep_punt - ep_fail) / (ep_go - ep_fail)
+
+# Print results
+cat("EP (Go For It):", ep_go_expected, "\n")
+cat("EP (Field Goal):", ep_fg, "\n")
+cat("EP (Punt):", ep_punt, "\n")
+cat("Estimated success probability (Go):", p_success, "\n")
+cat("Break-even probability (Go):", round(p_star, 3), "\n")
